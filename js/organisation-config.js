57221b303a93da26ddf2075606eb5bb3e1516224
samen-onder-handbereik <234217824+samen-onder-handbereik@users.noreply.github.com>
2026-01-29 12:03:24 +0100

    Edit4

--- js/organisation-config.js
+++ js/organisation-config.js
@@ -28,12 +28,6 @@ var organisationConfig = {
    latestVersion: ["nl_organisationPublishURL"],
    thisVersion: ["nl_organisationPublishURL"],
    prevVersion: ["nl_organisationPublishURL"],
-<<<<<<< HEAD
-// ????
-=======
-// ?
->>>>>>> 790ff27 (Edit3)
-
 
     useLogo: true,
     useLabel: true,
